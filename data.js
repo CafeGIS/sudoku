b7abd20998197e0e13195b27645b13fe25abac3e
Michal Olech <michal.olech@students.mimuw.edu.pl>
2012-02-24 12:07:10 +0100

    Board change prototype

---
+++ data.js
@@ -0,0 +1,50 @@
+var boards = [
+   [
+      "Plansza 1, bardzo łatwa",
+      "020730001" +
+      "009010047" +
+      "000208900" +
+      "000600802" +
+      "207853406" +
+      "804007000" +
+      "003405000" +
+      "640080700" +
+      "100072090"
+   ],
+   [
+      "Plansza 2, bardzo łatwa",
+      ""
+   ],
+   [
+      "Plansza 3, łatwa",
+      ""
+   ],
+   [
+      "Plansza 4, łatwa",
+      ""
+   ],
+   [
+      "Plansza 5, średnia",
+      ""
+   ],
+   [
+      "Plansza 6, średnia",
+      ""
+   ],
+   [
+      "Plansza 7, trudna",
+      ""
+   ],
+   [
+      "Plansza 8, trudna",
+      ""
+   ],
+   [
+      "Plansza 9, bardzo trudna",
+      ""
+   ],
+   [
+      "Plansza 10, bardzo trudna",
+      ""
+   ]
+];
--- logic.js
+++ logic.js
@@ -1,85 +1,175 @@
-var boardDesc = "020730001" +
-                "009010047" +
-                "000208900" +
-                "000600802" +
-                "207853406" +
-                "804007000" +
-                "003405000" +
-                "640080700" +
-                "100072090";
-
 var BOARD_SIZE = 9;
 
 function getDescFieldValue(boardDesc, x, y) {
    return boardDesc[x + y*BOARD_SIZE];
 }
 
-function getFieldValue($board, x, y) {
-   return $("#" +getFieldId(x, y), $board).html();
+function getFieldId(x, y) {
+   return 'field-' +x +'-' +y;
 }
 
-function getFieldId(x, y) {
-   return "field-" +x +"-" +y;
+function getFieldPos(fieldId) {
+   var e = /\d/g;
+   return fieldId.match(e);
+}
+
+function getField($board, fieldId) {
+   return $('#' +fieldId, $board);
+}
+
+function getCell(x, y) {
+   return Math.floor(x/3) + Math.floor(y/3) * 3;
 }
 
 function generateBoard($board, boardDesc) {
-   boardHtml = "<table cellspacing='0' cellpadding='0'>";
+   var boardHtml = '<table cellspacing="0" cellpadding="0">';
    
-   for (y = 0; y < BOARD_SIZE; ++y) {
-      boardHtml += "<tr>";
+   for (var y = 0; y < BOARD_SIZE; ++y) {
+      boardHtml += '<tr>';
 
-      for (x = 0; x < BOARD_SIZE; ++x) {
+      for (var x = 0; x < BOARD_SIZE; ++x) {
          val = getDescFieldValue(boardDesc, x, y);
 
-         boardHtml += "<td id='" +getFieldId(x, y) +"'>";
+         boardHtml += '<td class="field" id="' +getFieldId(x, y) +'">';
 
          if (val == 0) {
-            boardHtml += "<input type='text' class='write-field' />";
+            boardHtml += '<input type="text" class="write-field" />';
          } else {
-            boardHtml += "<input type='text' value='" +val +"' readonly='readonly' />";
+            boardHtml += '<input type="text" value="' +val 
+               +'" readonly="readonly" />';
          }
 
-         boardHtml += "</td>";
+         boardHtml += '</td>';
       }
 
-      boardHtml += "</tr>";
+      boardHtml += '</tr>';
    }
 
-   boardHtml += "</table>";
+   boardHtml += '</table>';
 
    $board.html(boardHtml);
 }
 
 function drawBorders($board) {
-   for (x = 0; x < BOARD_SIZE; ++x) {
-      for (y = 0; y < BOARD_SIZE; ++y) {
+   for (var x = 0; x < BOARD_SIZE; ++x) {
+      for (var y = 0; y < BOARD_SIZE; ++y) {
          if (x % 3 == 0 && x > 0)
-            $("#" +getFieldId(x, y), $board).css('border-left', '3px solid');
+            getField($board, getFieldId(x, y)).addClass('left-bordered');
          if (y % 3 == 0 && y > 0) 
-            $("#" +getFieldId(x, y), $board).css('border-top', '3px solid');
+            getField($board, getFieldId(x, y)).addClass('top-bordered');
       }
    }
 }
 
-function checkFields($board, fieldIds) {
-   exists = new Array(9);
+function getRowIds($board, y) {
+   var ids = new Array();
 
-   for (i = 0; i < fieldIds.length; ++i) {
-      value = getFieldValue($board, x, y);
+   for (var x = 0; x < BOARD_SIZE; ++x)
+      ids.push(getFieldId(x, y));
 
-      if (value < '0' && value > '9') return false;
-      if (exists[value]) return false;
-      exists[value] = true;
+   return ids;
+}
+
+function getColumnIds($board, x) {
+   var ids = new Array();
+
+   for (var y = 0; y < BOARD_SIZE; ++y)
+      ids.push(getFieldId(x, y));
+
+   return ids;
+}
+
+function getCellIds($board, c) {
+   var ids = new Array();
+
+   for (var i = 0; i < BOARD_SIZE; ++i) {
+      var x = (c*3) % BOARD_SIZE + i%3;
+      var y = Math.floor((c*3) / BOARD_SIZE)*3 + Math.floor(i/3);
+
+      ids.push(getFieldId(x, y));
+   }
+
+   return ids;
+}
+
+function highlightFields($board, fieldIds) {
+   for (var i in fieldIds) {
+      getField($board, fieldIds[i]).addClass('highlighted');
    }
 }
 
-function checkSolution($board) {
-   
+function highlight($board, fieldId) {
+   var pos = getFieldPos(fieldId);
+
+   highlightFields($board, getRowIds($board, pos[1]));
+   highlightFields($board, getColumnIds($board, pos[0]));
+   highlightFields($board, getCellIds($board, getCell(pos[0], pos[1])));
 }
 
-$(document).ready(function () {
+function unhighlight($board) {
+   $(".field", $board).removeClass('highlighted');
+}
+
+function checkFields($board, fieldIds) {
+   var fields = new Array(BOARD_SIZE);
+   var ok = true;
+
+   for (var i in fieldIds) {
+      $field = getField($board, fieldIds[i]);
+      value = $("input", $field).val();
+
+      if (value.length > 0 && (value < '0' || value > '9')) {
+         $field.addClass('conflicted');
+         ok = false;
+      }
+
+      if (typeof fields[value] == 'object') {
+         fields[value].addClass('conflicted');
+         $field.addClass('conflicted');
+         ok = false;
+      }
+
+      fields[value] = $field;
+   }
+
+   return ok;
+}
+
+function checkSolution($board) {
+   $(".field", $board).removeClass('conflicted');
+
+   for (var x = 0; x < BOARD_SIZE; ++x) {
+      for (var y = 0; y < BOARD_SIZE; ++y) {
+         checkFields($board, getRowIds($board, y));
+         checkFields($board, getColumnIds($board, x));
+         checkFields($board, getCellIds($board, getCell(x, y)));
+      }
+   }
+}
+
+$(document).ready(function(){
+   var $selection = $("div#selection");
    var $board = $("div#board");
 
-   generateBoard($board, boardDesc);
-   drawBorders($board);
+   var selectHtml = '<select>';
+   for (var i in boards)
+      selectHtml += '<option value="' +i +'">' +boards[i][0] +'</option>';
+   selectHtml += '</select>';
+
+   $selection.html(selectHtml);
+
+   $('select', $selection).change(function(){
+      var boardDesc = boards[$(this).val()][1];
+
+      generateBoard($board, boardDesc);
+      drawBorders($board);
+
+      $('.field input', $board).focus(function(){
+         highlight($board, $(this).parent().attr('id'));
+      }).blur(function(){
+         unhighlight($board);
+      }).keyup(function(){
+         checkSolution($board);
+      });
+   }).change();
 });
